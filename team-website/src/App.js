72bf3e8a250c0a1736366ba1383966df2ba3f0c7
Chirag Aswani <chirag@aswani.net>
2019-01-26 01:41:12 -0500

    start of the new semester :)

---
+++ team-website/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={"App"}>
+        <h1>PCT Tech Team Spring 2019 Website</h1>
+          <h4>Resources</h4>
+          <ul>
+              <li>GitHub: https://github.com/ChiragAswani/PCT-Tech-Team-Spring-2019</li>
+              <li>Google Drive: https://drive.google.com/drive/u/0/folders/0AN-dML6F20W8Uk9PVA</li>
+              <li>Project Repository: ____</li>
+          </ul>
+          <h4>Team</h4>
+          <ul>
+              <li>Chirag Aswani</li>
+              <li>Jason Cho</li>
+              <li>Tyler Donati</li>
+              <li>Jin-Young Bang</li>
+              <li>Ji Weiqi</li>
+              <li>Jennifer Roh</li>
+              <li>Randy Richardson</li>
+              <li>Justin Ching</li>
+          </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
