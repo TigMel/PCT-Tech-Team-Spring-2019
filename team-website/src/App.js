2fb2a81006ce9edbab4818c10e6e4a7a69d51c12
Chirag Aswani <chirag@aswani.net>
2019-01-27 17:04:41 -0500

    running

--- team-website/src/App.js
+++ team-website/src/App.js
@@ -24,6 +24,62 @@ class App extends Component {
               <li>Randy Richardson</li>
               <li>Justin Ching</li>
           </ul>
+          <h4>Trello Board</h4>
+          <blockquote className="trello-board-compact">
+              <a href="https://trello.com/b/OojRjvjP/pct-tech-team-spring-2019">Trello Board</a>
+          </blockquote>
+          <h2>The Fun Stuff!</h2>
+          <table>
+              <tr bgcolor="#0000ff" className="White">
+                  <td width="3%" bgcolor="#0066FF">
+                      <div align="center"><strong>Meeting Agenda </strong></div>
+                  </td>
+                  <td width="7%" bgcolor="#0066FF">
+                      <div align="center"><b>Homework Due</b></div>
+                  </td>
+              </tr>
+              <tr>
+                  <td>
+                      <h4>Meeting #1</h4>
+                      <ul>
+                          <li>Introduction to GitHub</li>
+                          <li>Project Proposals</li>
+                      </ul>
+                  </td>
+              </tr>
+              <tr>
+                  <td>
+                      <h4>Meeting #2 @8:00pm CAS</h4>
+                      <ul>
+                          <li>Decide on what project to build</li>
+                          <li>Lets Build an API! (each build a route on flask, use github to commit together)</li>
+                      </ul>
+                  </td>
+                  <td>
+                      <h4>Identify problems that need to be solved!</h4>
+                      <p>Examples</p>
+                      <ul>
+                          <li>Why does it take me 8 alarms to wake up?</li>
+                          <li>Why is the BU shuttle never accurate</li>
+                          <li>Ugh! I can't find the right song to play</li>
+                          <li>etc...</li>
+                      </ul>
+                      <p>How to go about this process</p>
+                      <ul>
+                          <li>My recommendation is to write down everything that frustrates you</li>
+                          <li><a href={'https://www.youtube.com/watch?v=9ArIwn4yImY'}>How do you find a problem worth solving?</a></li>
+                          <li><a href={'https://www.youtube.com/watch?v=xXMQGBhaync'}>How to Identify and Solve Problems</a></li>
+                      </ul>
+                      <h4>Install these...</h4>
+                      <ul>
+                          <li><a href={'https://www.getpostman.com/downloads/'}>Postman</a></li>
+                          <li>Python</li>
+                          <li><a href={'https://www.jetbrains.com/student/'}>Pycharm - Student Edition</a></li>
+                          <li><a href={'https://git-scm.com/book/en/v2/Getting-Started-Installing-Git'}>Git Command Tools</a></li>
+                      </ul>
+                  </td>
+              </tr>
+          </table>
       </div>
     );
   }
